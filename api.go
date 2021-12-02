7e3a56e7a1621566c935f1b9a469eba3dbd7ec5d
jayjhwu <jayjhwu@tencent.com>
2021-12-02 21:25:52 +0800

    feature: support register custom function

--- api.go
+++ api.go
@@ -2,7 +2,7 @@ package jmespath
 
 import "strconv"
 
-// JMESPath is the epresentation of a compiled JMES path query. A JMESPath is
+// JMESPath is the representation of a compiled JMES path query. A JMESPath is
 // safe for concurrent use by multiple goroutines.
 type JMESPath struct {
 	ast  ASTNode
@@ -47,3 +47,9 @@ func Search(expression string, data interface{}) (interface{}, error) {
 	}
 	return intr.Execute(ast, data)
 }
+
+func (jp *JMESPath) Register(f *FunctionEntry) {
+	jp.intr.fCall.functionTable[f.Name] = *f
+}
+
+
--- api_test.go
+++ api_test.go
@@ -1,10 +1,23 @@
 package jmespath
 
 import (
+	"encoding/json"
+	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/jmespath/go-jmespath/internal/testify/assert"
 )
+
+func TestValidUncompiledExpressionSearches(t *testing.T) {
+	assert := assert.New(t)
+	var j = []byte(`{"foo": {"bar": {"baz": [0, 1, 2, 3, 4]}}}`)
+	var d interface{}
+	err := json.Unmarshal(j, &d)
+	assert.Nil(err)
+	result, err := Search("foo.bar.baz[2]", d)
+	assert.Nil(err)
+	assert.Equal(2.0, result)
+}
 
 func TestValidPrecompiledExpressionSearches(t *testing.T) {
 	assert := assert.New(t)
@@ -30,3 +43,27 @@ func TestInvalidMustCompilePanics(t *testing.T) {
 	}()
 	MustCompile("not a valid expression")
 }
+
+func TestCustomFunction(t *testing.T) {
+	assert := assert.New(t)
+	data := make(map[string]interface{})
+	data["foo"] = "BAR"
+	precompiled, err := Compile("to_lower(foo)")
+	precompiled.Register(&FunctionEntry{
+		Name: "to_lower",
+		Arguments: []ArgSpec{
+			{Types: []JpType{JpString}},
+		},
+		Handler: func (arguments []interface{}) (interface{}, error) {
+			s := arguments[0].(string)
+			return strings.ToLower(s), nil
+		},
+	})
+
+	assert.Nil(err)
+	result, err := precompiled.Search(data)
+	assert.Nil(err)
+	assert.Equal("bar", result)
+}
+
+
